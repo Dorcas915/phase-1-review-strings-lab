695688b359819b1afd1289571f8cf90e11d13b4e
Dorcas Chepkoech <dorcaschepkoech717@gmail.com>
2025-09-04 21:05:15 +0300

    strings

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code in this file!
+const currentUser = "Dorcas";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+module.exports = {
+  currentUser,
+  welcomeMessage,
+  excitedWelcomeMessage,
+  shortGreeting
+};
